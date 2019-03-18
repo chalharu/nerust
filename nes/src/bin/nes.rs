a1eb358fe24acc9a9040642e52e8120f07fd371f
Mitsuharu Seki <mitsu1986@gmail.com>
2019-03-18 09:25:27 +0900

    LanMasterの追加

--- nes/src/bin/nes.rs
+++ nes/src/bin/nes.rs
@@ -18,6 +18,7 @@ fn main() {
         &mut include_bytes!(concat!(
             "../../../roms/",
             // "samples/sample1.nes",
+            "tests/Lan Master/Lan_Master.nes",
             // "giko005.nes",
             // "giko008.nes",
             // "giko009.nes",
@@ -40,7 +41,7 @@ fn main() {
             // "apu/test_apu_timers/dmc_pitch.nes"
             // "apu/apu_phase_reset/apu_phase_reset.nes",
             // "mapper/34_test_src/34_test_2.nes",
-            "ppu/ppu_sprite_hit/ppu_sprite_hit.nes"
+            // "ppu/ppu_sprite_hit/ppu_prite_hit.nes"
 
             // "color_test.nes",
             // "tests/tvpassfail/tv.nes",
@@ -63,7 +64,7 @@ fn main() {
             // "tests/scanline/scanline.nes",
             // "tests/nmi_sync/demo_ntsc.nes",
             // "tests/ntsc_torture.nes",
-        )).into_iter()
+        )).iter()
         .cloned(),
     ).unwrap();
 
---
+++ roms/tests/Lan Master/Source/bgm_game_dpcm.asm
@@ -0,0 +1,13 @@
+bgm_game_dpcm
+	.db $00+FT_DPCM_PTR,$05,$0f,$00
+	.db $00+FT_DPCM_PTR,$00,$00,$00
+	.db $02+FT_DPCM_PTR,$0a,$0f,$00
+	.db $00+FT_DPCM_PTR,$00,$00,$00
+	.db $05+FT_DPCM_PTR,$13,$0f,$00
+	.db $00+FT_DPCM_PTR,$00,$00,$00
+	.db $00+FT_DPCM_PTR,$00,$00,$00
+	.db $00+FT_DPCM_PTR,$00,$00,$00
+	.db $00+FT_DPCM_PTR,$00,$00,$00
+	.db $00+FT_DPCM_PTR,$00,$00,$00
+	.db $00+FT_DPCM_PTR,$00,$00,$00
+	.db $00+FT_DPCM_PTR,$00,$00,$00
