c698be9708c4632283e757130f9ce64399171001
chalharu <1978012+chalharu@users.noreply.github.com>
2026-06-03 09:21:18 +0900

    fix: update copyright information to chalharu in multiple files

--- core/src/lib.rs
+++ core/src/lib.rs
@@ -3,7 +3,7 @@
     reason = "emulator components include future mapper/APU hooks"
 )]
 
-// Copyright (c) 2018 Mitsuharu Seki
+// Copyright (c) 2018 chalharu
 //
 // This Source Code Form is subject to the terms of the Mozilla Public
 // License, v. 2.0. If a copy of the MPL was not distributed with this
