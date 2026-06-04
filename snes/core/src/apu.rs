afb7ba998e7d9e3c2ad35ce34c9da214a5ccc65b
chalharu <1978012+chalharu@users.noreply.github.com>
2026-06-05 07:39:56 +0900

    feat: update SMP instruction cycle calculations and add comprehensive tests

--- snes/core/src/apu.rs
+++ snes/core/src/apu.rs
@@ -1367,7 +1367,14 @@ impl Apu {
             0xAD => 2,
             0xBE => 3,
             0xDF => 3,
-            0xFE => 4,
+            0x6E => {
+                let address = self.peek_direct_address();
+                let value = self.peek_smp(address).wrapping_sub(1);
+                if value != 0 { 7 } else { 5 }
+            }
+            0xFE => {
+                if self.smp_y.wrapping_sub(1) != 0 { 6 } else { 4 }
+            }
             0xEF | 0xFF => 2,
             _ => 2,
         }
@@ -2048,7 +2055,7 @@ fn apu_port_index(offset: u16) -> usize {
 
 #[cfg(test)]
 mod tests {
-    use super::{Apu, DSP_KEY_ON, DSP_SOURCE_DIRECTORY};
+    use super::{Apu, DSP_KEY_ON, DSP_SOURCE_DIRECTORY, SMP_FLAG_C, SMP_FLAG_N, SMP_FLAG_V, SMP_FLAG_Z};
     use nerust_sound_traits::MixerInput;
 
     #[derive(Default)]
@@ -2069,6 +2076,158 @@ mod tests {
     fn write_dsp(apu: &mut Apu, register: usize, value: u8) {
         apu.write_smp(0x00F2, register as u8);
         apu.write_smp(0x00F3, value);
+    }
+
+    fn assert_opcode_cycles(apu: &Apu, opcode: u8, expected_cycles: u32) {
+        assert_eq!(
+            apu.smp_instruction_cycles(opcode),
+            expected_cycles,
+            "opcode {opcode:02X}"
+        );
+    }
+
+    fn assert_opcode_cycles_for_all(apu: &Apu, opcodes: &[u8], expected_cycles: u32) {
+        for &opcode in opcodes {
+            assert_opcode_cycles(apu, opcode, expected_cycles);
+        }
+    }
+
+    #[test]
+    fn smp_instruction_cycles_match_fullsnes_table() {
+        let mut apu = Apu::new();
+
+        assert_opcode_cycles(&apu, 0x00, 2);
+
+        assert_opcode_cycles_for_all(
+            &apu,
+            &[0x01, 0x11, 0x21, 0x31, 0x41, 0x51, 0x61, 0x71, 0x81, 0x91, 0xA1, 0xB1, 0xC1,
+                0xD1, 0xE1, 0xF1],
+            8,
+        );
+        assert_opcode_cycles_for_all(
+            &apu,
+            &[0x02, 0x12, 0x22, 0x32, 0x42, 0x52, 0x62, 0x72, 0x82, 0x92, 0xA2, 0xB2, 0xC2,
+                0xD2, 0xE2, 0xF2],
+            4,
+        );
+
+        apu.smp_pc = 0x0000;
+        apu.ram[0x0001] = 0x34;
+        apu.ram[0x0002] = 0x00;
+        apu.smp_a = 0x40;
+        apu.ram[0x0034] = 0x01;
+        assert_opcode_cycles_for_all(
+            &apu,
+            &[0x03, 0x23, 0x43, 0x63, 0x83, 0xA3, 0xC3, 0xE3],
+            7,
+        );
+        apu.ram[0x0034] = 0x00;
+        assert_opcode_cycles_for_all(
+            &apu,
+            &[0x13, 0x33, 0x53, 0x73, 0x93, 0xB3, 0xD3, 0xF3],
+            7,
+        );
+
+        assert_opcode_cycles_for_all(&apu, &[0x04, 0x24, 0x44, 0x64, 0x84, 0xA4, 0xC4, 0xE4], 3);
+        assert_opcode_cycles_for_all(&apu, &[0x05, 0x25, 0x45, 0x65, 0x85, 0xA5, 0xC5, 0xE5], 4);
+        assert_opcode_cycles_for_all(&apu, &[0x06, 0x26, 0x46, 0x66, 0x86, 0xA6, 0xC6, 0xE6], 3);
+        assert_opcode_cycles_for_all(&apu, &[0x07, 0x27, 0x47, 0x67, 0x87, 0xA7, 0xC7, 0xE7], 6);
+        assert_opcode_cycles_for_all(&apu, &[0x08, 0x28, 0x48, 0x68, 0x88, 0xA8, 0xC8, 0xE8], 2);
+        assert_opcode_cycles_for_all(&apu, &[0x09, 0x29, 0x49, 0x69, 0x89, 0xA9, 0xC9, 0xE9], 5);
+        assert_opcode_cycles_for_all(&apu, &[0x0A, 0x4A, 0x6A, 0x8A, 0xAA, 0xCA], 4);
+        assert_opcode_cycles_for_all(&apu, &[0x0B, 0x2B, 0x4B, 0x6B, 0x8B, 0xAB, 0xBB], 4);
+        assert_opcode_cycles_for_all(&apu, &[0x0C, 0x2C, 0x4C, 0x6C, 0x8C, 0xAC, 0xBC], 5);
+        assert_opcode_cycles_for_all(&apu, &[0x0D, 0x2D, 0x4D, 0x6D, 0x8D, 0xAE, 0xEE], 4);
+        assert_opcode_cycles_for_all(&apu, &[0x0E, 0x4E], 6);
+        assert_opcode_cycles(&apu, 0x0F, 8);
+
+        apu.smp_psw = 0;
+        assert_opcode_cycles(&apu, 0x10, 4);
+        apu.smp_psw = SMP_FLAG_N;
+        assert_opcode_cycles(&apu, 0x10, 2);
+        apu.smp_psw = SMP_FLAG_N;
+        assert_opcode_cycles(&apu, 0x30, 4);
+        apu.smp_psw = 0;
+        assert_opcode_cycles(&apu, 0x30, 2);
+        apu.smp_psw = 0;
+        assert_opcode_cycles(&apu, 0x50, 4);
+        apu.smp_psw = SMP_FLAG_V;
+        assert_opcode_cycles(&apu, 0x50, 2);
+        apu.smp_psw = SMP_FLAG_V;
+        assert_opcode_cycles(&apu, 0x70, 4);
+        apu.smp_psw = 0;
+        assert_opcode_cycles(&apu, 0x70, 2);
+        apu.smp_psw = 0;
+        assert_opcode_cycles(&apu, 0x90, 4);
+        apu.smp_psw = SMP_FLAG_C;
+        assert_opcode_cycles(&apu, 0x90, 2);
+        apu.smp_psw = SMP_FLAG_C;
+        assert_opcode_cycles(&apu, 0xB0, 4);
+        apu.smp_psw = 0;
+        assert_opcode_cycles(&apu, 0xB0, 2);
+        apu.smp_psw = 0;
+        assert_opcode_cycles(&apu, 0xD0, 4);
+        apu.smp_psw = SMP_FLAG_Z;
+        assert_opcode_cycles(&apu, 0xD0, 2);
+        apu.smp_psw = 0;
+        assert_opcode_cycles(&apu, 0xF0, 2);
+        apu.smp_psw = SMP_FLAG_Z;
+        assert_opcode_cycles(&apu, 0xF0, 4);
+
+        assert_opcode_cycles_for_all(&apu, &[0x14, 0x15, 0x16, 0x17, 0x34, 0x35, 0x36, 0x37,
+            0x54, 0x55, 0x56, 0x57, 0x74, 0x75, 0x76, 0x77, 0x94, 0x95, 0x96, 0x97, 0xB4, 0xB5,
+            0xB6, 0xB7, 0xD4, 0xD5, 0xD6, 0xD7, 0xF4, 0xF5, 0xF6, 0xF7], 4);
+        assert_opcode_cycles_for_all(&apu, &[0x18, 0x19, 0x38, 0x39, 0x58, 0x59, 0x78, 0x79,
+            0x98, 0x99, 0xB8, 0xB9], 5);
+        assert_opcode_cycles_for_all(&apu, &[0x1A, 0x3A], 6);
+        assert_opcode_cycles_for_all(&apu, &[0x1B, 0x3B, 0x5B, 0x7B], 5);
+        assert_opcode_cycles_for_all(&apu, &[0x1C, 0x3C, 0x5C, 0x7C, 0x9C, 0xDC, 0xFC, 0x1D,
+            0x3D, 0x5D, 0x7D, 0x9D, 0xBD, 0xDD, 0xFD, 0xBF, 0xCE], 2);
+        assert_opcode_cycles_for_all(&apu, &[0x1E, 0x3E, 0x5E], 4);
+        assert_opcode_cycles_for_all(&apu, &[0x1F, 0x5F], 3);
+        assert_opcode_cycles_for_all(&apu, &[0x20, 0x40, 0x60, 0x80, 0xA0, 0xC0, 0xE0, 0xED], 2);
+
+        apu.smp_pc = 0x0000;
+        apu.ram[0x0001] = 0x34;
+        apu.smp_a = 0x40;
+        apu.ram[0x0034] = 0x40;
+        assert_opcode_cycles(&apu, 0x2E, 5);
+        apu.ram[0x0034] = 0x41;
+        assert_opcode_cycles(&apu, 0x2E, 7);
+
+        apu.smp_x = 0;
+        apu.ram[0x0034] = 0x40;
+        assert_opcode_cycles(&apu, 0xDE, 5);
+        apu.ram[0x0034] = 0x41;
+        assert_opcode_cycles(&apu, 0xDE, 7);
+
+        assert_opcode_cycles(&apu, 0x2F, 4);
+        assert_opcode_cycles(&apu, 0x3F, 8);
+        assert_opcode_cycles(&apu, 0x4F, 6);
+        assert_opcode_cycles(&apu, 0x6F, 5);
+        assert_opcode_cycles(&apu, 0x7F, 6);
+        assert_opcode_cycles_for_all(&apu, &[0x8F, 0xAF, 0xFA], 5);
+        assert_opcode_cycles_for_all(&apu, &[0x9A, 0xBA], 5);
+        assert_opcode_cycles(&apu, 0x9E, 12);
+        assert_opcode_cycles(&apu, 0x9F, 5);
+        assert_opcode_cycles(&apu, 0xCF, 9);
+        assert_opcode_cycles(&apu, 0xAD, 2);
+        assert_opcode_cycles(&apu, 0xBE, 3);
+        assert_opcode_cycles(&apu, 0xDF, 3);
+
+        apu.smp_pc = 0x0000;
+        apu.ram[0x0001] = 0x34;
+        apu.ram[0x0034] = 0x01;
+        assert_opcode_cycles(&apu, 0x6E, 5);
+        apu.ram[0x0034] = 0x02;
+        assert_opcode_cycles(&apu, 0x6E, 7);
+
+        apu.smp_y = 0x01;
+        assert_opcode_cycles(&apu, 0xFE, 4);
+        apu.smp_y = 0x02;
+        assert_opcode_cycles(&apu, 0xFE, 6);
+
+        assert_opcode_cycles_for_all(&apu, &[0xEF, 0xFF], 2);
     }
 
     #[test]
