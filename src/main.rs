fbc0bd1166577a821319f4ee714adc8d5a6d28f8
chalharu <1978012+chalharu@users.noreply.github.com>
2026-06-27 21:37:01 +0900

    fix: streamline main function and improve error handling for backend and frontend selection

--- src/main.rs
+++ src/main.rs
@@ -7,17 +7,14 @@ fn create_factory() -> Box<dyn GpuFactory> {
     return Box::new(nerust_backend_opengl::GlFactory);
 }
 
-#[cfg(feature = "gtk")]
 fn main() {
+    #[cfg(feature = "gtk")]
     nerust_gtk::run(create_factory());
-}
-
-#[cfg(feature = "tao")]
-fn main() {
+    #[cfg(feature = "tao")]
     nerust_tao::run(create_factory());
 }
 
+#[cfg(not(any(feature = "wgpu", feature = "opengl")))]
+compile_error!("No backend selected. Enable feature 'wgpu' or 'opengl'.");
 #[cfg(not(any(feature = "gtk", feature = "tao")))]
-fn main() {
-    panic!("No frontend selected. Enable feature 'gtk' or 'tao'.");
-}
+compile_error!("No frontend selected. Enable feature 'gtk' or 'tao'.");
