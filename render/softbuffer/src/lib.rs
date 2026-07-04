8dfdd2e536e91c398ce5dd39a85e2bdbc786211f
chalharu <1978012+chalharu@users.noreply.github.com>
2026-07-04 10:50:20 +0900

    feat(softbuffer): refactor LUT structure and enhance bilinear resizing logic

--- render/softbuffer/src/lib.rs
+++ render/softbuffer/src/lib.rs
@@ -36,6 +36,7 @@ pub struct SoftbufferRenderer {
     render_profile: VideoRenderProfile,
     size: SurfaceSize,
     lut: LutEntry,
+    resize_buffer: Vec<u32>,
 }
 
 #[derive(Debug, Clone, Copy, PartialEq, Eq)]
@@ -47,9 +48,8 @@ enum ResizeKernel {
 // LUT (Look-Up Table) entry
 #[derive(Debug)]
 struct LutEntry {
-    x_lut: Vec<Vec<Option<usize>>>,
-    y_lut: Vec<Vec<Option<usize>>>,
-    weight_lut: Vec<Vec<Option<usize>>>, // 0 - 255 で重みを表す
+    x_lut: Vec<Option<(u16, u16)>>,
+    y_lut: Vec<Option<(u16, u16)>>,
     kernel: ResizeKernel,
 }
 
@@ -58,8 +58,8 @@ impl LutEntry {
         Self {
             x_lut: Vec::new(),
             y_lut: Vec::new(),
-            weight_lut: Vec::new(),
-            kernel: ResizeKernel::NearestNeighbor,
+            // kernel: ResizeKernel::NearestNeighbor,
+            kernel: ResizeKernel::Bilinear,
         }
     }
 
@@ -91,19 +91,9 @@ impl LutEntry {
         }
     }
 
-    fn lut_reserve(lut: &mut Vec<Vec<Option<usize>>>, len: usize, inner_len: usize) {
-        for (i, inner) in lut.iter_mut().enumerate() {
-            if i < len {
-                inner.reserve_exact(inner_len);
-                inner.clear();
-            }
-        }
-        while lut.len() < len {
-            lut.push(Vec::with_capacity(inner_len));
-        }
-        if lut.len() > len {
-            lut.truncate(len);
-        }
+    fn lut_reserve(lut: &mut Vec<Option<(u16, u16)>>, len: usize) {
+        lut.clear();
+        lut.reserve_exact(len);
     }
 
     fn resize_lut_nearest_neighbor(
@@ -114,18 +104,17 @@ impl LutEntry {
         src_h: usize,
         scale: (f32, f32),
     ) {
-        Self::lut_reserve(&mut self.x_lut, 1, dst_w);
-        Self::lut_reserve(&mut self.y_lut, 1, dst_h);
-        Self::lut_reserve(&mut self.weight_lut, 1, dst_h * dst_w);
+        Self::lut_reserve(&mut self.x_lut, dst_w);
+        Self::lut_reserve(&mut self.y_lut, dst_h);
 
         for x in 0..dst_w {
             // Center the source pixel coordinates based on the destination size and scaling factors
             let src_x = ((x as isize - (dst_w >> 1) as isize) as f32 * scale.0 + 0.5) as isize
                 + (src_w >> 1) as isize;
             if src_x < 0 || src_x >= src_w as isize {
-                self.x_lut[0].push(None);
+                self.x_lut.push(None);
             } else {
-                self.x_lut[0].push(Some(src_x as usize));
+                self.x_lut.push(Some((src_x as u16, 256)));
             }
         }
         for y in 0..dst_h {
@@ -133,18 +122,9 @@ impl LutEntry {
             let src_y = ((y as isize - (dst_h >> 1) as isize) as f32 * scale.1 + 0.5) as isize
                 + (src_h >> 1) as isize;
             if src_y < 0 || src_y >= src_h as isize {
-                self.y_lut[0].push(None);
+                self.y_lut.push(None);
             } else {
-                self.y_lut[0].push(Some(src_y as usize));
-            }
-        }
-        for y in 0..dst_h {
-            for x in 0..dst_w {
-                if self.x_lut[0][x].is_none() || self.y_lut[0][y].is_none() {
-                    self.weight_lut[0].push(None);
-                } else {
-                    self.weight_lut[0].push(Some(255));
-                }
+                self.y_lut.push(Some((src_y as u16, 256)));
             }
         }
     }
@@ -157,42 +137,73 @@ impl LutEntry {
         src_h: usize,
         scale: (f32, f32),
     ) {
-        Self::lut_reserve(&mut self.x_lut, 4, dst_w);
-        Self::lut_reserve(&mut self.y_lut, 4, dst_h);
-        Self::lut_reserve(&mut self.weight_lut, 4, dst_h * dst_w);
+        Self::lut_reserve(&mut self.x_lut, dst_w * 2);
+        Self::lut_reserve(&mut self.y_lut, dst_h * 2);
 
-        for y in 0..dst_h {
-            let src_y =
-                ((y as isize - (dst_h >> 1) as isize) as f32 * scale.1) + (src_h >> 1) as f32;
-            let diff_y = src_y - src_y.floor();
-            let base_y = src_y.floor() as isize;
-            for x in 0..dst_w {
-                let src_x =
-                    ((x as isize - (dst_w >> 1) as isize) as f32 * scale.0) + (src_w >> 1) as f32;
-                let diff_x = src_x - src_x.floor();
-                let base_x = src_x.floor() as isize;
+        for x in 0..dst_w {
+            // Center the source pixel coordinates based on the destination size and scaling factors
+            let src_x =
+                ((x as isize - (dst_w >> 1) as isize) as f32 * scale.0 + 0.5) + (src_w >> 1) as f32;
+            let src_x_floor = src_x.floor() as isize;
+            let src_x_ceil = src_x.ceil() as isize;
+            let mut weight_floor = ((src_x_ceil as f32 - src_x) * 256.0 + 0.5) as u16;
+            let mut weight_ceil = ((src_x - src_x_floor as f32) * 256.0 + 0.5) as u16;
 
-                for dy in 0..2 {
-                    for dx in 0..2 {
-                        let src_x = base_x + dx;
-                        let src_y = base_y + dy;
-                        let d = (dy * 2 + dx) as usize;
-                        if src_x < 0
-                            || src_x >= src_w as isize
-                            || src_y < 0
-                            || src_y >= src_h as isize
-                        {
-                            self.x_lut[d].push(None);
-                            self.y_lut[d].push(None);
-                            self.weight_lut[d].push(None);
-                        } else {
-                            self.x_lut[d].push(Some(src_x as usize));
-                            self.y_lut[d].push(Some(src_y as usize));
-                            let weight = ((1.0 - diff_x) * (1.0 - diff_y) * 255.0) as usize;
-                            self.weight_lut[d].push(Some(weight));
-                        }
-                    }
+            if src_x_floor < 0 || src_x_floor >= src_w as isize {
+                weight_floor = 0;
+                if weight_ceil != 0 {
+                    weight_ceil = 256;
                 }
+            }
+            if src_x_ceil < 0 || src_x_ceil >= src_w as isize {
+                if weight_floor != 0 {
+                    weight_floor = 256;
+                }
+                weight_ceil = 0;
+            }
+
+            if weight_floor == 0 {
+                self.x_lut.push(None);
+            } else {
+                self.x_lut.push(Some((src_x_floor as u16, weight_floor)));
+            }
+            if weight_ceil == 0 {
+                self.x_lut.push(None);
+            } else {
+                self.x_lut.push(Some((src_x_ceil as u16, weight_ceil)));
+            }
+        }
+        for y in 0..dst_h {
+            // Center the source pixel coordinates based on the destination size and scaling factors
+            let src_y =
+                ((y as isize - (dst_h >> 1) as isize) as f32 * scale.1 + 0.5) + (src_h >> 1) as f32;
+            let src_y_floor = src_y.floor() as isize;
+            let src_y_ceil = src_y.ceil() as isize;
+            let mut weight_floor = ((src_y_ceil as f32 - src_y) * 256.0 + 0.5) as u16;
+            let mut weight_ceil = ((src_y - src_y_floor as f32) * 256.0 + 0.5) as u16;
+
+            if src_y_floor < 0 || src_y_floor >= src_h as isize {
+                weight_floor = 0;
+                if weight_ceil != 0 {
+                    weight_ceil = 256;
+                }
+            }
+            if src_y_ceil < 0 || src_y_ceil >= src_h as isize {
+                if weight_floor != 0 {
+                    weight_floor = 256;
+                }
+                weight_ceil = 0;
+            }
+
+            if weight_floor == 0 {
+                self.y_lut.push(None);
+            } else {
+                self.y_lut.push(Some((src_y_floor as u16, weight_floor)));
+            }
+            if weight_ceil == 0 {
+                self.y_lut.push(None);
+            } else {
+                self.y_lut.push(Some((src_y_ceil as u16, weight_ceil)));
             }
         }
     }
@@ -215,8 +226,15 @@ impl LutEntry {
                 self.resize_lut_nearest_neighbor(dst_w, dst_h, src_w, src_h, scale);
             }
             ResizeKernel::Bilinear => {
-                // self.resize_lut_bilinear(dst_w, dst_h, src_w, src_h, scale);
+                self.resize_lut_bilinear(dst_w, dst_h, src_w, src_h, scale);
             }
+        }
+    }
+
+    fn lut_pixel_size(&self) -> usize {
+        match self.kernel {
+            ResizeKernel::NearestNeighbor => 1,
+            ResizeKernel::Bilinear => 2,
         }
     }
 }
@@ -232,6 +250,7 @@ impl SoftbufferRenderer {
             render_profile: profile.clone(),
             size: SurfaceSize::new(0, 0),
             lut: LutEntry::new(),
+            resize_buffer: Vec::new(),
         }
     }
 
@@ -243,6 +262,10 @@ impl SoftbufferRenderer {
             },
             1.0, // TODO: NTSCフィルタ適用後はアスペクト比が変わるので、ここで計算する必要がある
             self.size.clone(),
+        );
+        self.resize_buffer.resize(
+            self.size.width as usize * self.render_profile.source_logical_size.height as usize,
+            0,
         );
     }
 }
@@ -331,87 +354,91 @@ impl GpuRenderer for SoftbufferRenderer {
         let src_stride = frame.stride();
         let src = frame.as_ref();
         let dst = buffer.as_mut();
+        // let src_w = self.render_profile.source_logical_size.width as usize;
+        let src_h = self.render_profile.source_logical_size.height as usize;
 
-        match frame.format() {
-            PixelFormat::Rgba => {
-                for (i, (x_lut, y_lut)) in
-                    self.lut.x_lut.iter().zip(self.lut.y_lut.iter()).enumerate()
-                {
-                    for (y, &src_y) in y_lut.iter().enumerate() {
-                        let dst_y_index = y * dst_w;
-                        let Some(src_y) = src_y else {
-                            if i == 0 {
-                                for x in 0..dst_w {
-                                    dst[dst_y_index + x] = 0;
-                                }
-                            }
-                            continue;
-                        };
-                        let src_y_index = src_y * src_stride;
-                        for (x, &src_x) in x_lut.iter().enumerate() {
-                            let dst_index = dst_y_index + x;
-                            let Some(src_x) = src_x else {
-                                dst[dst_index] = 0;
-                                continue;
-                            };
-                            let src_index = src_y_index + src_x * 4;
-
-                            let c = u32::from_le_bytes([
-                                src[src_index + 2], // Blue
-                                src[src_index + 1], // Green
-                                src[src_index],     // Red
-                                src[src_index + 3], // Alpha
-                            ]);
-                            if i == 0 {
-                                dst[dst_index] = c;
-                            } else {
-                                dst[dst_index] += c;
-                            }
-                        }
-                    }
-                }
-            }
+        let src_f: Box<dyn Fn(usize) -> [u8; 4]> = match frame.format() {
+            PixelFormat::Rgba => Box::new(|i| src[i * 4..i * 4 + 4].try_into().unwrap()),
             PixelFormat::PaletteIndex { palette } => {
-                // if self.ntsc_packed_rgba8.is_none() {
-                for (i, (x_lut, y_lut)) in
-                    self.lut.x_lut.iter().zip(self.lut.y_lut.iter()).enumerate()
-                {
-                    for (y, &src_y) in y_lut.iter().enumerate() {
-                        let dst_y_index = y * dst_w;
-                        let Some(src_y) = src_y else {
-                            if i == 0 {
-                                for x in 0..dst_w {
-                                    dst[dst_y_index + x] = 0;
-                                }
-                            }
-                            continue;
-                        };
-                        let src_y_index = src_y * src_stride;
-                        for (x, &src_x) in x_lut.iter().enumerate() {
-                            let dst_index = dst_y_index + x;
-                            let Some(src_x) = src_x else {
-                                dst[dst_index] = 0;
-                                continue;
-                            };
-                            let src_index = src_y_index + src_x;
-                            let c = palette[src[src_index] as usize];
-                            // Convert from 0xRRGGBBAA to 0xAARRGGBB for softbuffer
-                            if i == 0 {
-                                dst[dst_index] = c.rotate_right(8);
-                            } else {
-                                dst[dst_index] += c.rotate_right(8);
-                            }
-                        }
-                    }
-                }
-                // } else {
-                // let source_size = LogicalSize {
-                //     width: src_w,
-                //     height: src_h,
-                // };
-                // let mut filter = FilterType::NtscComposite.generate(source_size);
-                // filter.push(value, filter_func);
-                // }
+                Box::new(|i| palette[src[i] as usize].to_le_bytes())
+            }
+        };
+
+        // 横方向に拡大
+        for y in 0..src_h {
+            let dst_y_index = y * dst_w;
+            let src_y_index = y * src_stride;
+            for x in 0..dst_w {
+                let dst_index = dst_y_index + x;
+                let lut_index = x * self.lut.lut_pixel_size();
+                let c = self.lut.x_lut[lut_index..lut_index + self.lut.lut_pixel_size()]
+                    .into_iter()
+                    .filter_map(|&x| x)
+                    .map(|(i, w)| {
+                        let src_index = src_y_index + i as usize;
+                        let src_val = src_f(src_index);
+                        // 重みを適用する
+                        [
+                            ((src_val[1] as u16 * w as u16 + 0x80) >> 8) as u8, // Blue
+                            ((src_val[2] as u16 * w as u16 + 0x80) >> 8) as u8, // Green
+                            ((src_val[3] as u16 * w as u16 + 0x80) >> 8) as u8, // Red
+                            ((src_val[0] as u16 * w as u16 + 0x80) >> 8) as u8, // Alpha
+                        ]
+                    })
+                    .reduce(|acc, val| {
+                        // 4チャンネルの色を加算する
+                        [
+                            acc[0].saturating_add(val[0]), // Blue
+                            acc[1].saturating_add(val[1]), // Green
+                            acc[2].saturating_add(val[2]), // Red
+                            acc[3].saturating_add(val[3]), // Alpha
+                        ]
+                    })
+                    .map(|v| u32::from_le_bytes(v));
+
+                let Some(c) = c else {
+                    self.resize_buffer[dst_index] = 0;
+                    continue;
+                };
+                self.resize_buffer[dst_index] = c;
+            }
+        }
+
+        // 縦方向に拡大
+        for x in 0..dst_w {
+            for y in 0..dst_h {
+                let lut_index = y * self.lut.lut_pixel_size();
+                let dst_index = y * dst_w + x;
+                let c = self.lut.y_lut[lut_index..lut_index + self.lut.lut_pixel_size()]
+                    .into_iter()
+                    .filter_map(|&x| x)
+                    .map(|(i, w)| {
+                        let src_index = i as usize * dst_w + x;
+                        let src_val = self.resize_buffer[src_index].to_le_bytes();
+                        // 重みを適用する, 横方向に拡大した結果を縦方向に拡大するので、横方向の色順序はそのまま使う
+                        [
+                            ((src_val[0] as u16 * w as u16 + 0x80) >> 8) as u8, // Blue
+                            ((src_val[1] as u16 * w as u16 + 0x80) >> 8) as u8, // Green
+                            ((src_val[2] as u16 * w as u16 + 0x80) >> 8) as u8, // Red
+                            ((src_val[3] as u16 * w as u16 + 0x80) >> 8) as u8, // Alpha
+                        ]
+                    })
+                    .reduce(|acc, val| {
+                        // 4チャンネルの色を加算する
+                        [
+                            acc[0].saturating_add(val[0]), // Blue
+                            acc[1].saturating_add(val[1]), // Green
+                            acc[2].saturating_add(val[2]), // Red
+                            acc[3].saturating_add(val[3]), // Alpha
+                        ]
+                    })
+                    .map(|v| u32::from_le_bytes(v));
+
+                let Some(c) = c else {
+                    dst[dst_index] = 0;
+                    continue;
+                };
+                dst[dst_index] = c;
             }
         }
 
