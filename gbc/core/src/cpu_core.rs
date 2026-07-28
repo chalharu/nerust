036044d1f41ba9af73cec40248f0acb2e32ff39c
chalharu <1978012+chalharu@users.noreply.github.com>
2026-07-28 22:58:55 +0000

    fix(gbc): CGB L=0x0D (same as AGB)

--- gbc/core/src/cpu_core.rs
+++ gbc/core/src/cpu_core.rs
@@ -62,7 +62,7 @@ impl Lr35902Cpu {
                 cpu.registers.set_l(0x4D);
             }
             GbcModel::Cgb => {
-                // CGB post-boot: A=0x11, B=0x00, Z=1
+                // CGB post-boot: A=0x11, B=0x00, L=0x0D, Z=1
                 cpu.registers.set_a(0x11);
                 cpu.registers.set_f(0xB0);
                 cpu.registers.set_b(0x00);
@@ -70,7 +70,7 @@ impl Lr35902Cpu {
                 cpu.registers.set_d(0xFF);
                 cpu.registers.set_e(0x56);
                 cpu.registers.set_h(0x00);
-                cpu.registers.set_l(0x00);
+                cpu.registers.set_l(0x0D);
             }
             GbcModel::Agb => {
                 // AGB (GBA GBC mode): A=0x11, F=0x00, B=0x01, L=0x0D
